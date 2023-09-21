1f9f500e5c72ce357864c814d87123a2c7e6739a
NafisAamir <aamirnafis82@gmail.com>
2023-09-21 19:00:47 +0530

    final-commit

--- src/Components/BottomSheet.jsx
+++ src/Components/BottomSheet.jsx
@@ -1,44 +1,86 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './BottomSheet.css';
-import { AiFillCloseSquare } from 'react-icons/ai'
+import { AiFillCloseSquare } from 'react-icons/ai';
 
 const BottomSheet = () => {
-    const [sheetState, setSheetState] = useState('closed');
+  const [sheetState, setSheetState] = useState('closed');
+  const [dragging, setDragging] = useState(false);
+  const [initialMouseY, setInitialMouseY] = useState(0);
 
-    const toggleSheet = () => {
-        switch (sheetState) {
-            case 'closed':
-                setSheetState('half-open');
-                break;
-            case 'half-open':
-                setSheetState('fully-open');
-                break;
-            case 'fully-open':
-                setSheetState('closed');
-                break;
-            default:
-                setSheetState('closed');
-        }
-    };
-
-    const handleCloseClick = () => {
+  const toggleSheet = () => {
+    switch (sheetState) {
+      case 'closed':
+        setSheetState('half-open');
+        break;
+      case 'half-open':
+        setSheetState('fully-open');
+        break;
+      case 'fully-open':
         setSheetState('closed');
+        break;
+      default:
+        setSheetState('closed');
+    }
+  };
+
+  const handleCloseClick = () => {
+    setSheetState('closed');
+  };
+
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      if (dragging) {
+        const deltaY = e.clientY - initialMouseY;
+        
+        const threshold1 = 50;
+        // const threshold2 = 100;
+        if (deltaY > threshold1 ) {
+          setSheetState('half-open');
+        } else if (deltaY < -threshold1) {
+          setSheetState('fully-open');
+        }
+        // else if(deltaY < -threshold1){
+        //     setSheetState('closed');
+        // }
+
+      }
     };
 
-    return (
-        <div className={`bottom-sheet ${sheetState}`}>
-            <div className="sheet-header">
-                <div className="handle" onClick={toggleSheet}></div>
-                <div className="close-button-container">
-                    <AiFillCloseSquare onClick={handleCloseClick} className="close-button" />
-                </div>
-            </div>
-            <div className="content">
-                <h1 style={{display:"flex", justifyContent:"center"}}>Made by Nafis Aamir</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos minima, repellat vitae amet deleniti accusantium? Aliquid odio asperiores eius tempora perspiciatis nobis voluptatem exercitationem repellendus! Aut temporibus ipsa hic sit qui. Ex molestias porro, provident doloremque fugiat magnam molestiae consequuntur aliquam et laudantium sit facilis accusamus ipsa optio magni laborum unde, non deserunt. Ducimus possimus excepturi iste consectetur laboriosam voluptatibus voluptate culpa, unde sit, nulla eum porro dignissimos obcaecati dicta? Quam maiores beatae asperiores dolor nisi quaerat. Iste dolor omnis repellat quidem ullam ut deleniti repellendus laudantium tenetur! Modi delectus, praesentium repellendus distinctio dicta recusandae magnam impedit totam veritatis, similique fuga voluptatibus! Distinctio expedita vel blanditiis, perspiciatis amet quae laborum quisquam minus voluptatibus ipsa repellat unde assumenda! Assumenda magni eius tempora iusto in, quas dolor non ex impedit veniam illum, quis harum quasi exercitationem totam alias dignissimos sit suscipit laboriosam aspernatur molestiae expedita! Quam omnis, assumenda, cupiditate nemo officia dolor veritatis enim repellat eligendi laboriosam exercitationem nesciunt sint praesentium optio odit odio vero aliquam iste perspiciatis voluptatum excepturi blanditiis rerum voluptates! Esse, quasi mollitia ea tempore laborum adipisci eius doloremque aut sapiente soluta cupiditate pariatur quia amet, aperiam, possimus magni nisi. Perspiciatis suscipit culpa nulla sed quia repudiandae itaque asperiores totam, deserunt harum eum ut, rem optio officiis aliquid? Dicta omnis, architecto expedita amet maiores commodi dolorum labore explicabo ex, rem corrupti quisquam, id eaque pariatur beatae tenetur? Consequatur itaque, aliquid sint laborum enim repellendus dolor nesciunt, unde, quis odio cumque provident tenetur suscipit ex accusantium? Et inventore aperiam id minus nostrum repudiandae quaerat praesentium amet. Tempora rem aliquid ipsa, debitis quae maxime neque minima pariatur reiciendis vel hic eum cupiditate deleniti ex cum accusantium dolores consectetur natus alias quia voluptatibus molestiae quam aut delectus. Veniam ut omnis modi aperiam sed. Exercitationem est pariatur enim error numquam odio in alias veniam sit totam unde amet dolorem dolores animi, vitae maiores assumenda tempore, distinctio tenetur cupiditate, illo iusto. A quas saepe esse totam quaerat fugiat placeat, possimus perspiciatis ex nisi rem assumenda quam ut! Suscipit, eos. Iure a vel ipsam reprehenderit delectus cupiditate inventore nesciunt nihil hic exercitationem totam odit, voluptates earum ea. Ut eveniet praesentium, repellendus reiciendis repellat tempore vero totam, magni consequatur dolorem, earum veniam impedit! Cumque est quis velit alias nesciunt necessitatibus iure autem iste repellat, aut temporibus natus, deserunt eaque ipsa vero animi aliquid repudiandae ab delectus illo laboriosam officiis vel aliquam eius! Ducimus magni nemo natus id voluptatum dicta facere nulla doloribus quis distinctio, ipsum enim similique. Qui pariatur nobis quis, in, recusandae eius sint magni sequi commodi libero enim quia saepe? Dolore quo soluta distinctio ipsam inventore omnis quasi aliquid quas. Magnam distinctio exercitationem harum ullam ratione molestiae officiis nostrum, maiores provident quasi quos impedit nam sint dolores autem quaerat, sit doloremque. Dolore provident repellat quas? Reprehenderit ea cum officiis asperiores? Voluptatum illum ipsa ab exercitationem nam. Consequatur rerum ea provident non possimus in nostrum minus, architecto velit neque, tenetur ipsum commodi beatae voluptas doloremque exercitationem quidem vitae, expedita atque. Doloribus nihil ab non neque.</p>
-            </div>
+    const handleMouseUp = () => {
+      if (dragging) {
+        setDragging(false);
+      }
+    };
+
+    document.addEventListener('mousemove', handleMouseMove);
+    document.addEventListener('mouseup', handleMouseUp);
+
+    return () => {
+      document.removeEventListener('mousemove', handleMouseMove);
+      document.removeEventListener('mouseup', handleMouseUp);
+    };
+  }, [dragging, initialMouseY]);
+
+  return (
+    <div className={`bottom-sheet ${sheetState}`}>
+      <div
+        className="sheet-header"
+        onMouseDown={(e) => {
+          setDragging(true);
+          setInitialMouseY(e.clientY);
+        }}
+      >
+        <div className="handle" onClick={toggleSheet}></div>
+        <div className="close-button-container">
+          <AiFillCloseSquare onClick={handleCloseClick} className="close-button" />
         </div>
-    );
+      </div>
+      <div className="content">
+        <h1>Made by Nafis Aamir</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam commodi facilis recusandae earum qui voluptas autem esse enim adipisci. Reiciendis reprehenderit facere delectus voluptate eum veniam. Exercitationem repudiandae alias mollitia quibusdam dolor aliquam laboriosam assumenda ea voluptatem pariatur tempore modi veritatis dolorum debitis tempora, eligendi labore hic ex. Nemo, libero harum aliquid eligendi ullam neque, eveniet nesciunt consectetur officia ducimus sapiente provident ea exercitationem soluta. Deleniti vero sed magni molestiae assumenda incidunt, id ad iure aspernatur consequuntur quasi, cumque numquam quis a ratione hic. Sint expedita illo officiis odit libero, illum eos facilis hic quis natus ipsum voluptate eaque molestiae soluta maiores aliquid! Sapiente quia veniam suscipit eligendi vero, libero et. Ipsum quas quis laboriosam! Provident doloribus nam perferendis debitis possimus ut commodi, doloremque ab aperiam a? Facere corrupti cupiditate atque in itaque, nemo non officiis minima suscipit, corporis, velit unde ea libero iusto dignissimos soluta. Tenetur officiis recusandae quos praesentium itaque, sint laborum dignissimos iusto eos aspernatur assumenda dolorem quo tempora ratione quaerat pariatur, modi, odit placeat veniam. Dolore tempore quod magnam ea minima repudiandae commodi, iste repellendus rerum, vero quae reiciendis quam recusandae nulla modi numquam alias, voluptatem exercitationem labore odit itaque reprehenderit earum officiis minus? Quo sed sunt, doloribus vero, exercitationem consequatur odit quaerat eaque, cumque impedit fuga dicta asperiores vitae natus assumenda id consectetur dolores totam ullam maxime quasi eveniet ab? Eaque fugit ipsa sunt. Laboriosam illo sed maxime, eos illum odit? Quaerat optio nostrum, incidunt totam molestias animi labore, similique adipisci ratione, aspernatur dolores molestiae dignissimos voluptates quidem corporis sit placeat saepe expedita iusto quibusdam. Commodi, vel, nisi unde repudiandae distinctio mollitia ipsum enim omnis ab id fuga consequatur corporis? Quis autem molestias dignissimos quos, et voluptas doloremque iusto, excepturi necessitatibus eaque porro explicabo, accusantium ducimus nulla pariatur eum magni quod voluptatum! Error aliquid repellat asperiores voluptates perspiciatis optio. Sit quae dignissimos ea! Eaque vero aut, cum dolorum, reiciendis aspernatur consequatur sit officiis cumque odit totam, ipsum libero excepturi alias molestias! Eum ullam obcaecati animi aperiam nulla modi provident, sit quasi ipsa harum eligendi, assumenda rerum aliquid nam vero! Voluptate praesentium sit adipisci. Aperiam dolores quidem laborum possimus sapiente accusamus placeat, quaerat neque est omnis odit minima optio cum dolor, temporibus quae provident aliquam quibusdam quis odio laudantium. Aperiam, quisquam perferendis. Harum voluptas officia quis neque doloremque molestias id, sint dolorem minima atque repellendus aperiam itaque ut. Tempora similique ex aut, sapiente sunt error minus assumenda, recusandae laudantium perspiciatis voluptatum animi voluptatem autem a facere perferendis ipsa vel laboriosam soluta numquam, consequuntur iure aliquam debitis vero. Pariatur maxime dolor porro sequi ad. Vitae similique doloribus quaerat quos? Quo doloremque, tenetur quisquam illum harum corporis? Non dicta quis est amet debitis fugiat? Quasi consequatur dignissimos voluptatum aliquid. Aut ipsam doloremque expedita architecto non facilis perferendis eos dolor cupiditate voluptatibus itaque nam unde delectus, in nulla adipisci a ad minima impedit pariatur atque! Temporibus sint doloremque assumenda. Dolor, explicabo sequi? Deserunt, nisi nostrum magnam beatae atque officiis non. Totam accusantium illo quisquam eveniet facere culpa sed eius?</p>
+      </div>
+    </div>
+  );
 };
 
 export { BottomSheet };
